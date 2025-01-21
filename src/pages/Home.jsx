50e6b5822b7157ea873fb1c0debabfda9079c98b
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-01-22 02:31:09 +0600

    f

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import LatestCollection from '../components/LatestCollection';
+import PopunderAd from '../components/PopUnderadd';
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <PopunderAd/>
       <LatestCollection/>
     </div>
   )
