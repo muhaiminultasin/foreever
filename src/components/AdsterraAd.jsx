01b56cbfbefbeb6dd2cee8116236e84db91fe77c
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-01-22 14:06:44 +0600

    f

--- src/components/AdsterraAd.jsx
+++ src/components/AdsterraAd.jsx
@@ -7,12 +7,11 @@ const AdsterraAd = () => {
     script1.type = "text/javascript";
     script1.innerHTML = `
       atOptions = {
-        'key' : '930a3b1c968d41d6495796abfb61d276',
-        'format' : 'iframe',
-        'height' : 300,
-        'width' : 160,
-        'params' : {}
-      };
+		'key' : '930a3b1c968d41d6495796abfb61d276',
+		'format' : 'iframe',
+		'height' : 300,
+		'width' : 160,
+		'params' : {}
     `;
 
     // Create the second script to load the external script
