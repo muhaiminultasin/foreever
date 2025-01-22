e221fe6fd86c14bb3feb099981e33211ea9ed724
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-01-22 14:10:54 +0600

    f

--- src/components/AdsterraAd.jsx
+++ src/components/AdsterraAd.jsx
@@ -7,17 +7,19 @@ const AdsterraAd = () => {
     script1.type = "text/javascript";
     script1.innerHTML = `
       atOptions = {
-		'key' : '930a3b1c968d41d6495796abfb61d276',
-		'format' : 'iframe',
-		'height' : 300,
-		'width' : 160,
-		'params' : {}
+  'key': '930a3b1c968d41d6495796abfb61d276',
+  'format': 'iframe',
+  'height': 300,
+  'width': 160,
+  'params': {}
+}
     `;
 
     // Create the second script to load the external script
     const script2 = document.createElement("script");
     script2.type = "text/javascript";
-    script2.src = "//www.highperformanceformat.com/930a3b1c968d41d6495796abfb61d276/invoke.js";
+    script2.src =
+      "//www.highperformanceformat.com/930a3b1c968d41d6495796abfb61d276/invoke.js";
     script2.async = true;
 
     // Append both scripts to the ad container
